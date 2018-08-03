e85ad8b304320b5b4d10b207d3f9f342dd7e42b2
Monica Lisa Reyes <mlisareyes@gmail.com>
2018-08-03 04:36:59 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -44,7 +44,10 @@ class Student
     self.id = load_data[0]
   end
 
-  def self.create
+  def self.create(name, grade)
+    student = Student.new(name, grade)
+    student.save
+    student
   end
 
 
